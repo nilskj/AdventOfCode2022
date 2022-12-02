c9f0677cf42f0a4e38d1d5a786adb4c1e72e1588
nilskj <nilskjellman@gmail.com>
2022-12-02 08:07:33 +0100

    optimise part A

--- day02a.py
+++ day02a.py
@@ -2,29 +2,15 @@ def solve(lines):
     scores = 0
     for opponent, your_move in lines:
 
-        # Sten
-        if your_move == "X":
-            if opponent == "A":
-                scores += 3
-            elif opponent == "C":
-                scores += 6
-            scores += 1
+        diff_opponent = ord(opponent) - ord("A")
+        diff_your_move = ord(your_move) - ord("X")
 
-        # Sax
-        elif your_move == "Y":
-            if opponent == "B":
-                scores += 3
-            elif opponent == "A":
-                scores += 6
-            scores += 2
-
-        # Påse
-        elif your_move == "Z":
-            if opponent == "C":
-                scores += 3
-            elif opponent == "B":
-                scores += 6
+        if diff_opponent == diff_your_move:
             scores += 3
+        if (diff_your_move - 1) % 3 == diff_opponent:
+            scores += 6
+
+        scores += diff_your_move + 1
 
     return scores
 
